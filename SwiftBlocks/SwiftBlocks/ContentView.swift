fb2ba1560f043ab16ede307247aba4d964632b3c
Hiep Nguyen <hiep@Hieps-MacBook-Pro.local>
2021-09-14 19:55:50 -0700

    Add nav bar

--- SwiftBlocks/SwiftBlocks/ContentView.swift
+++ SwiftBlocks/SwiftBlocks/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Text("Test")
+                }
+            }
+                .navigationBarTitle(Text("swiftBlocks"))
+        }
     }
 }
 
