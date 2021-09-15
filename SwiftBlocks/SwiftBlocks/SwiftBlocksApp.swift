9c1b4d646aecbe84c124b593427d6201e92da103
Hiep Nguyen <hiep@Hieps-MacBook-Pro.local>
2021-09-14 19:29:21 -0700

    Add ios app

---
+++ SwiftBlocks/SwiftBlocks/SwiftBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBlocksApp.swift
+//  SwiftBlocks
+//
+//  Created by Hiep Nguyen on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
